0786630034e2639a88a3f53eefe4a84b32569ec6
richardtokar <richardtokar@gmail.com>
2024-07-19 22:39:37 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rich_Code
+ Rich's code samples
---
+++ test.py
@@ -0,0 +1,13 @@
+# Parentheses
+def add(a, b):
+    return a + b
+
+result = add(1, 2)  # Function call
+
+# Brackets
+my_list = [1, 2, 3, 4]
+second_element = my_list[1]  # Indexing
+
+# Braces
+my_dict = {'name': 'Alice', 'age': 30}
+name = my_dict['name']  # Accessing dictionary value
